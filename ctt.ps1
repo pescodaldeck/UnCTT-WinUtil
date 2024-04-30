c007acd4fc677805a6d0ab7116d76ff46d5b2845
ErSupremoLaziale <152210963+pescodaldeck@users.noreply.github.com>
2024-04-30 22:10:39 +0200

    Update ctt.ps1
    Added a custom warning when running UnCTT WinUtil without Administrator privileges.

--- ctt.ps1
+++ ctt.ps1
@@ -1,1 +1,8 @@
-iwr -useb https://christitus.com/win | iex
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "You must run UnCTT WinUtil as Administrator." -ForegroundColor Red
+    Write-Host "Closing with exit code 1 ..."
+    Start-Sleep -Seconds 1
+    exit 1
+}
+
+iwr https://christitus.com/win | iex
